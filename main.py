d6ea7b052ab06ac03ddc2c466e0ef50eb3117d71
nemanja33 <djokic.nemanja97@gmail.com>
2025-07-05 12:49:22 +0200

    WIP printing responses to the console

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
 def main():
-    print("Hello from ai-agent!")
+    api_key = os.getenv("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    model_name = "gemini-2.0-flash-001"
+    prompt = sys.argv[1]
+    verbose = sys.argv.pop() == "--verbose"
+    messages = [
+        genai.types.Content(
+            role="user",
+            parts=[genai.types.Part(text=prompt)]
+        )
+    ]
+    
+    response = client.models.generate_content(
+        model=model_name,
+        contents=messages
+    )
+    
+    print("Response text:")
+    print(response.text)
+
+    if verbose:
+        print(f"\nUser prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
